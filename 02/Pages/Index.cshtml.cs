127f11bc6ed1e1c86d8af968b7ab36e6d04a030a
ContatoEmanuel <contato.emanuel97@gmail.com>
2023-07-17 19:25:45 -0300

    Exercício 2

---
+++ 02/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _02.Pages;
+
+public class IndexModel : PageModel
+{
+    public string? MeuTexto {get; set;}
+
+    public void OnPost([FromForm] string meuTexto){
+        MeuTexto = meuTexto;
+    }
+
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
