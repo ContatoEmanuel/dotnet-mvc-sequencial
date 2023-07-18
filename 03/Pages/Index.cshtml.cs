a51cf5584c85b1209217e56465248de1c5edfa31
ContatoEmanuel <contato.emanuel97@gmail.com>
2023-07-18 20:03:21 -0300

    Exercício 3

---
+++ 03/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _03.Pages;
+
+public class IndexModel : PageModel
+{
+    public int? Result {get; set;}
+
+    public void OnPost([FromForm] int num1, int num2){
+        Result = num1 + num2;
+    }
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
